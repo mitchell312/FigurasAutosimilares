e5a87f426e738941aa4205146fc6607fe3b03480
Olvea Mendoza Patrick Samuel <polvea@unsa.edu.pe>
2026-05-29 16:20:51 -0500

    cambio de donde inicia la tortuga y hacia donde mira al iniciar e inclusion del llamado de la funcion fractaltree y el retroceso de la tortuga para que pueda dibujar la rama derecha

--- examples/testLine.c
+++ examples/testLine.c
@@ -1,5 +1,5 @@
 #include "../turtlec.h"
-void fractaltree(turtle *, float, int);
+void fractaltree(Turtle *, float, int);
 int main(void){
   TurtleApp *app = turtleAppCreate(400, 200, "Test Line");
 
@@ -9,12 +9,15 @@ int main(void){
   Turtle *t = turtleAppGetTurtle(app);
 
   turtlePenUp(t);
-  turtleGoTo(t, 50.0f, 100.0f);
+  turtleGoTo(t, 200.0f, 200.0f);
   turtlePenDown(t);
 
   turtleSetColor(t, 255, 100, 0);
   turtleSetSpeed(t, 5.0f);
-  turtleForward(t, 300.0f);
+  
+
+  int level = 2;
+  fractaltree(t, 100, level);
 
   turtleAppRun(app);
   turtleAppDestroy(app);
@@ -28,5 +31,8 @@ void fractaltree(Turtle* t, float len, int level){
   turtleLeft(t, 45);
   fractaltree(t, len * 0.9, level - 1);
   turtleRight(t, 90);
-  fractaltree(t, len 0*9, level - 1);
+  fractaltree(t, len * 0.9, level - 1);
+  turtleRight(t, 135);
+  turtleForward(t, len);
+  turtleRight(t, 180);
 }
